f0cf2687c6d03c5b3ef380281d4b7cbc9901e094
Russell Owen <rowen@uw.edu>
2014-06-30 16:45:00 -0700

    Added an example batch job and improved the snow program to halt the axes when stopped.

---
+++ job/example.py
@@ -0,0 +1,23 @@
+"""Example batch job
+"""
+
+MSPerSec = 1000
+WaitSec = 2
+
+def run(sr):
+    """Simple test batch job
+    """
+    yield sr.waitCmd('broadcast "example batch job showing time"')
+    yield sr.waitCmd("show time")
+    yield sr.waitCmd('broadcast "example batch job waiting %s seconds"' % (WaitSec,))
+    yield sr.waitMS(WaitSec * MSPerSec)
+    yield sr.waitCmd('broadcast "example batch job showing focus"')
+    yield sr.waitCmd("show focus")
+
+
+def end(sr):
+    """Called after the job ends (whether it ends normally, or stopped, or fails)
+
+    Do NOT yield or call sr.wait... methods in the end function!
+    """
+    sr.startCmd('broadcast "example batch job end function called"')
--- job/snow.py
+++ job/snow.py
@@ -1,16 +1,20 @@
-SecPerMS = 1000
+MSPerSec = 1000
 
-_WaitMS = 900 * SecPerMS
-_WarnDelayMS = 10 * SecPerMS
+_WaitMS = 900 * MSPerSec
+_WarnDelayMS = 10 * MSPerSec
 
 def run(sr):
     """Snow program batch job
     """
     while True:
+        yield sr.waitCmd('broadcast/type=warning "snow program starting up"')
         for az in (135, 270, 45, 180, 315, 90, 225, 0):
             yield sr.waitCmd("track %s, 65 mount" % (az,))
             yield sr.waitMS(_WaitMS)
-#            yield sr.waitCmd("broadcast \"snow program moving to next position\"")
+            yield sr.waitCmd('broadcast/type=warning "snow program moving to next position"')
             # make a small warning move, first
             yield sr.waitCmd("track %s, 65 mount" % (az - 0.1,))
             yield sr.waitMS(_WarnDelayMS)
+
+def end(sr):
+    sr.startCmd("axis init")
