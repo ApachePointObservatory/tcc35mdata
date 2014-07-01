c696ea7745f36a7750a695e941645dbd3ec9897f
Russell Owen <rowen@uw.edu>
2014-07-01 12:03:42 -0700

    Revert telescope model and tweak snow program
    Reverted the telescope model to the old TCC's version (negated, as usual)
    because mirrors are now being commanded the same as the old TCC.
    Fixed a bug in snow program.

--- jobs/snow.py
+++ jobs/snow.py
@@ -1,5 +1,6 @@
-_WaitSec = 900
-_WarnDelaySec = 10
+Alt=75
+WaitSec = 900
+WarnSec = 10
 
 def run(sr):
     """Snow program batch job
@@ -7,12 +8,12 @@ def run(sr):
     yield sr.waitCmd('broadcast/type=warning "snow program starting up"')
     while True:
         for az in (135, 270, 45, 180, 315, 90, 225, 0):
-            yield sr.waitCmd("track %s, 65 mount" % (az,))
-            yield sr.waitSec(_WaitSec)
+            yield sr.waitCmd("track %s, %s mount" % (az, Alt))
+            yield sr.waitSec(WaitSec)
             yield sr.waitCmd('broadcast/type=warning "snow program moving to next position"')
             # make a small warning move, first
-            yield sr.waitCmd("track %s, 75 mount" % (az - 0.1,))
-            yield sr.waitSec(_WarnDelaySec)
+            yield sr.waitCmd("track %s, %s mount" % (az - 0.1, Alt))
+            yield sr.waitSec(WarnSec)
 
 def end(sr):
     sr.startCmd("axis init")
