248f26b249f51d75528ae3c4cde0836b26a8f571
Russell Owen <rowen@uw.edu>
2014-07-01 11:57:08 -0700

    Tweak snow program: alt=75 (as requested by Bill) and use waitSet instead of waitMS

--- jobs/snow.py
+++ jobs/snow.py
@@ -1,20 +1,18 @@
-MSPerSec = 1000
-
-_WaitMS = 900 * MSPerSec
-_WarnDelayMS = 10 * MSPerSec
+_WaitSec = 900
+_WarnDelaySec = 10
 
 def run(sr):
     """Snow program batch job
     """
+    yield sr.waitCmd('broadcast/type=warning "snow program starting up"')
     while True:
-        yield sr.waitCmd('broadcast/type=warning "snow program starting up"')
         for az in (135, 270, 45, 180, 315, 90, 225, 0):
             yield sr.waitCmd("track %s, 65 mount" % (az,))
-            yield sr.waitMS(_WaitMS)
+            yield sr.waitSec(_WaitSec)
             yield sr.waitCmd('broadcast/type=warning "snow program moving to next position"')
             # make a small warning move, first
-            yield sr.waitCmd("track %s, 65 mount" % (az - 0.1,))
-            yield sr.waitMS(_WarnDelayMS)
+            yield sr.waitCmd("track %s, 75 mount" % (az - 0.1,))
+            yield sr.waitSec(_WarnDelaySec)
 
 def end(sr):
     sr.startCmd("axis init")
